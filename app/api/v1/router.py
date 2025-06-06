97b629da741b3e29eb53039321845aef412bb0f2
komronium <komronium@gmail.com>
2025-06-06 17:17:09 +0500

    remove E-Imzo

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users, profile, otp, eimzo,ad,category
+from app.api.v1.endpoints import auth, users, profile, otp, ad, category
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(otp.router)
-api_router.include_router(eimzo.router)
 api_router.include_router(users.router)
 api_router.include_router(profile.router)
 api_router.include_router(ad.router)
--- app/schemas/eimzo.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Base64Dto(BaseModel):
-    data: str
